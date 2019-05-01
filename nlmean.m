0a528d8b5daa566dfe5d3f7085b3ba2f5f23ad27
Bhushan Suwal <bhushan_suwal@live.com>
2019-05-01 13:20:36 -0400

    code that broke array limit

--- nlmean.m
+++ nlmean.m
@@ -1,18 +1,18 @@
 %% Preprocessing
 % img = importdata('Indian_pines.mat');
 % modified_img = importdata('Indian_pines.mat');
-
+% loadlibrary('get_dist.dll','get_dist.h')
 img = imread('lena.png');
 img = imnoise(img,'gaussian');
 modified_img = imread('lena.png');
 % crop the img to have a reasonable weight matrix
 
 % make the patches and display them first
-patch_size = 7; % must always be odd numbered
+patch_size = 1; % must always be odd numbered
 [height, width, dims] = size(img);
 
 % search window
-window_size = 21; 
+window_size = 3; 
 
 % padding is the distance (in num pixels) from the center pixel to the 
 % boundary of the patch
@@ -21,30 +21,32 @@ search_padding = floor(window_size/2);
 
 % this is only the number of patches within the search window
 num_patches = (height - 2 * patch_padding) * (width - 2 * patch_padding);
+patches = load(strcat(num2str(patch_size), '_patch_size.mat'));
+patches = double(patches.patches);
 
 fprintf('Number of patches %i\n', num_patches);
 
 %% Building the weight matrix
 fprintf('Building weight matrix ...\n');
 
-dists = zeros(num_patches, window_size);
-h = 8;
+dists = Inf(num_patches, window_size);
+h = 1;
 row = 0;
 
 tic
 profile on
 
-for i = patch_padding + 1: width - patch_padding
-    for j = patch_padding + 1: height - patch_padding
+for j = patch_padding + 1: height - patch_padding
+    for i = patch_padding + 1: width - patch_padding
         % get the main patch
-        main_center = [i,j];
-        main_patch = get_patch(img, main_center, patch_size);
-        
+%         main_center = [i,j];
         row = row + 1;
+        main_patch = patches(row, :);
+       
         col = 0;
         % compare other patches with patch
-        for k = i - search_padding: i + search_padding
-            for l = j - search_padding: j + search_padding
+        for l = j - search_padding: j + search_padding
+            for k = i - search_padding: i + search_padding
                 % get the compare patch
                  col = col + 1;
                  
@@ -55,14 +57,14 @@ for i = patch_padding + 1: width - patch_padding
                      || l > height - patch_padding        % down bound
                      continue % ignore out of bounds patches
                  end
-             
-                 compare_center = [k,l];
-                 compare_patch = get_patch(img, compare_center, patch_size);
+ 
+                 compare_patch = get_patch(img, k, l, patch_padding, patch_size);
                 
                 % get the distance between patches
-                 dist = get_distance(main_patch, compare_patch);                   
+                dist = get_distance(main_patch, compare_patch, patch_size, dims); 
+%                  dist = get_dist(main_patch, int_compare_patch, patch_size^2);
                 % populate the weights
-                 dists(row, col) = exp(-dist./(h^2));       
+                dists(row, col) = exp(-dist./(h^2));       
             end
         end 
     end
@@ -100,38 +102,11 @@ imwrite(modified_img, 'modified_lena_noised.png')
 
 
 %%
-function window = get_search_window(img, center_pixel, window_size)
-    padding = floor(window_size/2);
-    [width, height, dims] = size(img);
-    center_x = center_pixel(1);
-    center_y = center_pixel(2);
-    
-    window = zeros(window_size * window_size, dims);
-    pixel_count = 0;
-    % compare other patches with patch
-    for k = center_x - padding: center_x + padding
-        for l = center_y - padding: center_y + padding
-            
-            pixel_count = pixel_count + 1;
-            % boundary checks for search window
-            if  k < 1 ...             % left bound
-                || l <  1 ...         % up bound
-                || k > width  ...     % right bound
-                || l > height         % down bound
-                continue % ignore out of bounds patches
-            end
-
-            window(pixel_count, :) = img(k,l, :);
-        end
-    end
-    
-end
-
 function pixel = get_weighted_pixel(window, weights_vec, window_size, dims)
     pixel = weights_vec * reshape(window, [window_size * window_size, dims]) ;
 end
 
-function patch = get_patch(img, center_pixel, patch_size)
+function patch = get_patch(img, center_x, center_y, padding, patch_size)
     % Returns a patch from the image
     % Args: img          - the image
     %       center_pixel - the pixel in the center of the patch
@@ -139,29 +114,17 @@ function patch = get_patch(img, center_pixel, patch_size)
     %
     % patch_size is always odd
     % It is assumed that the patch is withing the dimensions of the image
-    
-    center_x = center_pixel(1);
-    center_y = center_pixel(2);
-    padding = (patch_size-1)/2; 
 
     patch = img(center_x - padding: center_x + padding, ...
                 center_y - padding: center_y + padding, ...
                 :);
 end
 
-function dist = get_distance(uint_patch1, uint_patch2)
+function dist = get_distance(main_patch, uint_patch2, patch_size, dims)
     % Returns the euclidean distance between two patches 
-    [~, patch_size, dims] = size(uint_patch1);
-    % the patches are of dtype uint8, we need to convert them to int8 so 
-    % x - y != 0 when x < y
-    int_patch1 = int16(reshape(uint_patch1, [patch_size * patch_size * dims, 1]));
-    int_patch2 = int16(reshape(uint_patch2, [patch_size * patch_size * dims, 1]));
-    % get the euclidean distance between the patches
-    squared = ((int_patch1 - int_patch2).^2);              
+    int_patch2 = double(reshape(uint_patch2, [patch_size * patch_size * dims, 1]));
+    
+    % dist = pdist2(main_patch, int_patch2');
+    squared = ((main_patch - int_patch2).^2);              
     dist = sqrt(sum(squared(:)));        
 end
-
-
-
-
-
